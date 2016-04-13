267c7e8f0002096dd9e906c438af4ec22225c0c0
HowardStark <howard@getcoffee.io>
2016-04-13 15:50:19 -0700

    Removed unecessary assignments

--- abreuvoir.go
+++ abreuvoir.go
@@ -12,13 +12,11 @@ var (
 // SetAddress sets the address of the remote server
 func SetAddress(newAddress string) {
 	address = newAddress
-	_ = address
 }
 
 // SetPort sets the port of the remote server
 func SetPort(newPort string) {
 	port = newPort
-	_ = port
 }
 
 // InitClient initializes the client and the connection to the remote server.
