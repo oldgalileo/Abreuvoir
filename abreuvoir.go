a0d5f42a2253086b2df6aef72988d1684549a8bb
HowardStark <howard@getcoffee.io>
2016-04-13 15:40:46 -0700

    Start of NetworkTables implementation

---
+++ abreuvoir.go
@@ -0,0 +1,27 @@
+package abreuvoir
+
+import (
+	"fmt"
+)
+
+var (
+	address, port string = "0.0.0.0", "1735"
+	client        Client
+)
+
+// SetAddress sets the address of the remote server
+func SetAddress(newAddress string) {
+	address := newAddress
+	_ = address
+}
+
+// SetPort sets the port of the remote server
+func SetPort(newPort string) {
+	port := newPort
+	_ = port
+}
+
+// InitClient initializes the client and the connection to the remote server.
+func InitClient() {
+	fmt.Println(address, port)
+}
---
+++ client.go
@@ -0,0 +1,3 @@
+package abreuvoir
+
+type Client struct{}
