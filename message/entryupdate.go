981fcb4226697df61d556282d5caee2e6c18095c
HowardStark <howard@getcoffee.io>
2016-04-14 23:41:12 -0700

    Added more helper functions to EntryUpdate

--- message/entryupdate.go
+++ message/entryupdate.go
@@ -8,22 +8,32 @@ type EntryUpdate struct {
 	update entry.Update
 }
 
+func EntryUpdateFromUpdate(entryUpdate entry.Update) *EntryUpdate {
+	return &EntryUpdate{
+		update: entryUpdate,
+		Base: Base{
+			mType: typeEntryUpdate,
+			mData: entryUpdate.CompressToBytes(),
+		},
+	}
+}
+
 // EntryUpdateFromBytes builds an EntryUpdate message from an entry
 func EntryUpdateFromItems(entryId [2]byte, entrySeq [2]byte, entryType byte, entryData []byte) (*EntryUpdate, error) {
 	tempUpdate, err := entry.UpdateFromItems(entryId, entrySeq, entryType, entryData)
-    if err != nil {
-        return nil, err
-    }
-    return &EntryUpdate{
-        update: tempUpdate,
-        Base: Base{
-            mType: typeEntryUpdate,
-            mData: tempUpdate.CompressToBytes()
-        }
-    }, nil
+	if err != nil {
+		return nil, err
+	}
+	return &EntryUpdate{
+		update: tempUpdate,
+		Base: Base{
+			mType: typeEntryUpdate,
+			mData: tempUpdate.CompressToBytes(),
+		},
+	}, nil
 }
 
 // GetUpdate returns the Update associated with this EntryUpdate
 func (entryUpdate *EntryUpdate) GetUpdate() *entry.Update {
-    return entryUpdate.update
+	return entryUpdate.update
 }
