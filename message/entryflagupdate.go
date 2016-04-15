fd87da6e978852bafa22e966f31f2baf8c31269a
HowardStark <howard@getcoffee.io>
2016-04-14 23:39:03 -0700

    Added better message helper functions

--- message/entryflagupdate.go
+++ message/entryflagupdate.go
@@ -8,19 +8,29 @@ type EntryFlagUpdate struct {
 	flagUpdate entry.FlagUpdate
 }
 
+func EntryFlagUpdateFromFlagUpdate(flagUpdateData entry.FlagUpdate) *EntryFlagUpdate {
+	return &EntryFlagUpdate{
+		update: flagUpdateData,
+		Base: Base{
+			mType: typeEntryFlagUpdate,
+			mData: flagUpdateData.CompressToBytes(),
+		},
+	}
+}
+
 // EntryFlagUpdateFromBytes builds an EntryFlagUpdate message using the provided parameters
 func EntryFlagUpdateFromItems(entryId [2]byte, entryFlag byte) *EntryFlagUpdate {
 	tempFlagUpdate := entry.FlagUpdateFromItems(entryId, entryFlag)
-    return &EntryFlagUpdate{
-        update: tempFlagUpdate,
-        Base: Base{
-            mType: typeEntryFlagUpdate,
-            mData: tempFlagUpdate.CompressToBytes()
-        }
-    }
+	return &EntryFlagUpdate{
+		update: tempFlagUpdate,
+		Base: Base{
+			mType: typeEntryFlagUpdate,
+			mData: tempFlagUpdate.CompressToBytes(),
+		},
+	}
 }
 
 // GetFlagUpdate returns the Update associated with this EntryFlagUpdate
 func (entryUpdate *EntryFlagUpdate) GetFlagUpdate() *entry.FlagUpdate {
-    return entryUpdate.flagUpdate
+	return entryUpdate.flagUpdate
 }
